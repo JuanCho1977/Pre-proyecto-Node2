7a094fab8bab8e659a889ee6da20dee73d95d086
JuanCho1977 <gonzalezinsfranjm@gmail.com>
2024-08-26 11:45:53 -0300

    Noveno Commit

--- src/router/index.js
+++ src/router/index.js
@@ -3,10 +3,11 @@ const productRouter = require('./api/productRouter.js')
 const cartsRouter = require('./api/cartsRouter.js')
 const userRoute = require ('./api/userRouter.js')
 const menssageRouter = require('./api/MenssageRouter.js')
+const viewRouter  = require ('./router/viewsRouter.js')
 
 const router =  Router()
 
-//router.use('/', () => {})
+router.use('/', viewRouter)
 router.use('/api/products', productRouter)
 router.use('/api/carts', cartsRouter)
 router.use('/api/user', userRoute)
--- src/server.js
+++ src/server.js
@@ -1,7 +1,9 @@
 
 const express   =  require ('express')
+const handlebars =  require ('express-handlebars')
 const routerApp =  require('./router/index.js')
 const { connectDb } = require('./config/index.js')
+
 
 const app       = express()
 const PORT      = 8080
@@ -10,6 +12,13 @@ const PORT      = 8080
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 connectDb()
+
+
+app.engine('handlebars',handlebars.engine)
+app.set('views', __dirname+'/views')
+app.set('view engine', 'handlebars')
+
+
 app.use(routerApp)
 
 
