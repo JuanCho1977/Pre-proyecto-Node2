328cb81ab7aef87d4ab4ec67869283873e63dfec
JuanCho1977 <gonzalezinsfranjm@gmail.com>
2024-09-01 00:46:45 -0300

    catorceavo commit

---
+++ src/daos/Memory/memory.js
@@ -0,0 +1,25 @@
+
+class MemoryDatabase {
+    constructor() {
+        this.cart = []  
+
+    }
+    addProductToCart(productId) {
+        this.cart.push(productId)
+        return this.cart;
+    }
+
+    
+    removeProductFromCart(productId) {
+        this.cart = this.cart.filter(id => id !== productId)
+        return this.cart
+    }
+
+    
+    getCart() {
+        return this.cart
+    }
+}
+
+
+    module.exports = { MemoryDatabase }
--- src/router/viewsRouter.js
+++ src/router/viewsRouter.js
@@ -1,34 +1,90 @@
 const { Router } = require('express')
 const { cartManagerMongo } = require('../../src/daos/Momgo/cartManagerMongo.js')
 const { productManagerMongo } = require('../../src/daos/Momgo/productManagerMongo.js')
-const mongoose = require('mongoose');
+const { MemoryDatabase } = require('../../src/daos/Memory/memory.js')
+
+
+const mongoose = require('mongoose'); 
 const cartService = new cartManagerMongo()
 const productService = new productManagerMongo()
+const memoryDatabase = new MemoryDatabase()
 const router = Router()
-
 
 router.post('/carts', async (req, res) => {
     try {
-        console.log("estoy en el POST de Carritos")
+        console.log("Estoy en el POST de Carritos");
         const { productId } = req.body;
 
-        console.log(`ID de producto recibido: ${productId }`)
-        
-        const carts = await cartService.createcart(productId )
-          
-        if (!carts || carts.length == 0) {
-            return res.status(404).send({ status: 'error', message: 'No se encontraron carritos' })
+        console.log(`ID de producto recibido: ${productId}`);
 
+        
+        const carts = memoryDatabase.addProductToCart(productId);
+
+        if (!carts || carts.length === 0) {
+            return res.status(404).send({ status: 'error', message: 'No se encontraron productos en el carrito' });
         }
-        res.render('carts', {
+
+        // voy al memory
+        const products = await productService.getProducts(); // Cambia a obtener todos los productos
+        const cartProducts = products.filter(product => carts.includes(product._id.toString()));
+
+        res.render('product', {
             title: 'Carritos de Compras',
-            carts: carts
-        }) 
+            products: cartProducts 
+        });
     } catch (ERROR) {
         console.log('Error:', ERROR);
-        res.status(500).send({ status: 'error', message: 'Error al obtener los carritos' })
+        res.status(500).send({ status: 'error', message: 'Error al obtener los productos del carrito' });
     }
-})
+});
+
+
+router.post('/carts/delete', async (req, res) => {
+    try {
+        console.log("Estoy en el DELETE de Carritos");
+        const { productId } = req.body;
+
+        console.log(`ID de producto a eliminar: ${productId}`);
+
+        
+        const carts = memoryDatabase.removeProductFromCart(productId);
+
+        // Obtengo los detalles del producto desde la base de datos MEMORY
+        const products = await productService.getProducts();
+        const cartProducts = products.filter(product => carts.includes(product._id.toString()));
+
+        res.render('product', {
+            title: 'Carritos de Compras',
+            products: cartProducts
+        })
+    } catch (ERROR) {
+        console.log('Error:', ERROR);
+        res.status(500).send({ status: 'error', message: 'Error al eliminar el producto del carrito' });
+    }
+});
+router.post('/carts/data', async (req, res) => {
+    try {
+        console.log("ingrese al POST DATA de Carrito")
+        const { productId  } = req.body;
+            if (!productId ) {
+                return res.status(400).send({ status: 'error', message: 'Debe completar los campos obligatorios' });
+            }
+            console.log(` ID POST DATA de Carrito ${productId}`)
+            const carts = memoryDatabase.removeProductFromCart(productId)
+            const products = await productService.getProducts()
+            const cartProducts = products.filter(product => carts.includes(product._id.toString()))
+            const resCart = await cartService.createcart(cartProducts)
+            console.log(cartProducts)
+            res.render('product', {
+            title: 'Carritos de Compras enviado'
+            })
+           
+            res.send({ status: 'success', payload: resCart })
+    } catch (ERROR) {
+        console.log('Error:', ERROR);
+        res.status(500).send({ status: 'error', message: 'Error al crear el carrito' });
+    }
+});
 
 router.get('/products/search', async (req, res) => {
   try {
